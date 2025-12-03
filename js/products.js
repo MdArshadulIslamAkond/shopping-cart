fb7f20f061e24f3344fda90a68c20fd2a7bc4856
Arshadul-Islam <arshadul36@gmail.com>
2025-12-04 00:46:08 +0600

    some picture add

--- js/products.js
+++ js/products.js
@@ -6,7 +6,6 @@ const products = [
     name: "Smart Watch",
     description: "Waterproof, Fitness Tracker",
     price: 40,
-    // image: "https://via.placeholder.com/200",
     image: "https://www.applegadgetsbd.com/_next/image?url=https%3A%2F%2Fadminapi.applegadgetsbd.com%2Fstorage%2Fmedia%2Fmedium%2FApple-Watch-Series-11-jet-black-3902.jpg&w=3840&q=100",
   },
   {
@@ -14,7 +13,6 @@ const products = [
     name: "Headphone",
     description: "Noise Cancelling Wireless",
     price: 30,
-    // image: "https://via.placeholder.com/200",
     image: "https://vibegaming.com.bd/wp-content/uploads/2024/03/White.png",
   },
   {
@@ -22,7 +20,6 @@ const products = [
     name: "Bluetooth Speaker",
     description: "HD Sound Quality",
     price: 25,
-    // image: "https://via.placeholder.com/200",
     image: "https://img.drz.lazcdn.com/static/bd/p/37f00f6d4895701c379f677d0f314435.jpg_720x720q80.jpg",
   },
 ];
